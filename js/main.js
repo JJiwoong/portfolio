571eaf37c51afb507a641e03079f43ec4701978e
jiwoo <jeongjw0520@naver.com>
2024-07-16 13:35:06 +0900

    24.07.16 upgrade4

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
 gsap.registerPlugin(ScrollTrigger);
 
+let currentCookie = document.cookie; //쿠키를 가져오는 방법
+let cookieCheck = currentCookie.indexOf('jiwoo');
 
-let noticeElement = document.querySelector(".notice");
-let closeBtn = document.querySelector(".close");
+let noticeElement = document.querySelector(".notice")
+let checkboxElement = document.querySelector("#cb");
 
-closeBtn.addEventListener("click", function () {
-  noticeElement.style.display = "none";
-});
+let closeBtn=document.querySelector(".close")
+
+
+if (cookieCheck > -1) {
+    noticeElement.style.display = "none"
+} else {
+    noticeElement.style.display = "block"
+}
+
+checkboxElement.addEventListener("change", function () {
+    //오늘의 날짜
+    let date = new Date();
+    date.setDate(date.getDate() + 7)
+
+    if (checkboxElement.checked) { //input에 check가 되었다면
+        //쿠키 생성하기
+        let setCookie = "";
+        setCookie += "jiwoo=true; ";
+        setCookie += "expires=" + date.toUTCString();
+
+        //쿠키 저장하기
+        document.cookie = setCookie;
+    }
+})
+
+closeBtn.addEventListener("click",function(){
+  noticeElement.style.display="none"
+   
+})
+
+
 
 
 function typing_() {
