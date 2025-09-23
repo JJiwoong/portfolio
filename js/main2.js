11223b027fcc2cb03d303e027778eb155c8571a5
JJiwoong <jeongjw0520@naver.com>
2025-09-23 18:20:24 +0900

    3

--- js/main2.js
+++ js/main2.js
@@ -67,7 +67,7 @@ let tl2 = gsap.timeline();
       scrollTrigger: {
           trigger: "#about_introduce>h2>span",
           start: "top bottom",
-          end: "+=300% top",
+          end: "+=450% top",
           scrub: 0.5,
       },
       color: "#fff",
