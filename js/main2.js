8a661dc097a009ea71ba8d254aead5fffdd48d8d
JJiwoong <jeongjw0520@naver.com>
2025-10-01 15:49:24 +0900

    1

--- js/main2.js
+++ js/main2.js
@@ -173,7 +173,7 @@ gsap.fromTo(".introduction_txt p",
     duration: 1,
     ease: "power3.out",
     scrollTrigger: {
-      trigger: ".about_personal",
+      trigger: ".introduction_wrap",
       start: "60% bottom",   // about 섹션의 top이 뷰포트 60% 지점에 닿을 때 시작
       end: "bottom 40%",  // 필요시 조정
       toggleActions: "play none none reverse",
