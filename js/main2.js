da36e397f849eb1a6219146a00f7d0480523e8a7
JJiwoong <jeongjw0520@naver.com>
2025-10-01 14:55:54 +0900

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -96,7 +96,7 @@ snb.addEventListener("transitionend", () => {
 gsap.to(".snb-wrap", {
   scrollTrigger: {
     trigger: ".about_visual",
-    start: "bottom 20%",
+    start: "top top",
     end: "bottom top",
     toggleActions: "play none none reverse",
     onEnter: () => {
