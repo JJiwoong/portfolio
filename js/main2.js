28a05f2997dfca8894e2f8e968a70cb256a3efb2
JJiwoong <jeongjw0520@naver.com>
2025-10-02 01:53:30 +0900

    1

--- js/main2.js
+++ js/main2.js
@@ -165,7 +165,22 @@ gsap.fromTo(".introduction_txt p",
   }
 );
 
+document.querySelectorAll(".group").forEach((group, i) => {
+  const panels = group.querySelectorAll(".panel");
 
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: group,
+      start: "top top",
+      end: "+=100%", // 한 화면 동안 고정
+      scrub: true,
+      pin: true, // 그룹 고정
+    }
+  })
+  .to(panels[0], { y: "0%", opacity: 1, duration: 0.5 })
+  .to(panels[1], { y: "0%", opacity: 1, duration: 0.5 })
+  .to(panels[2], { y: "0%", opacity: 1, duration: 0.5 });
+});
 // const list = document.querySelector(".skills_list");
 // const totalWidth = list.scrollWidth;
 
