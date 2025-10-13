cd870b06ede3d894f86c72bcb2090b5db42d038f
JJiwoong <jeongjw0520@naver.com>
2025-10-14 00:27:12 +0900

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -209,7 +209,7 @@ gsap.fromTo(".work_project",
     ease: "power3.out",
     scrollTrigger: {
       trigger: ".project_wrap",
-      start: "top 80%",   // about 섹션의 top이 뷰포트 60% 지점에 닿을 때 시작
+      start: "top 10%",   // about 섹션의 top이 뷰포트 60% 지점에 닿을 때 시작
       end: "bottom 40%",  // 필요시 조정
       toggleActions: "play none none reverse",
       markers:true,
