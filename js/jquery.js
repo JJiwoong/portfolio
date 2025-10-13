20d6fe36dcfb5869c248e5536e1341c25369b83d
JJiwoong <jeongjw0520@naver.com>
2025-10-14 01:11:30 +0900

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -53,33 +53,19 @@ $('a').mouseleave(function(){
   gsap.to('.cursor2 span', { visibility: "hidden", opacity: 0,duration:0.1})
 })
 
-// 배경 클래스 교체를 항상 '배타적'으로
-function setBodyBG(cls) {
-  document.body.classList.remove('white-bg', 'black-bg');
-  document.body.classList.add(cls);
-}
-
-$('[data-color]').each(function () {
-  const colorValue = $(el).data('color');          // "#fff" or "#000"
-  const colorClass = (colorValue === '#fff') ? 'white-bg' : 'black-bg';
-
-  ScrollTrigger.create({
-    trigger: "#contact",                 // ← 이 el은 data-color 가진 section (여기선 #contact)
-    start: "top 55%",            // 범위를 조금 안쪽으로 (너무 일찍 on 되는 것 방지)
-    end:   "bottom 45%",
-    scrub: false,                // 색 전환은 딱- 바뀌게, 원하면 1로
-    anticipatePin: 1,            // 상단 pin 섹션 영향 보정
-    markers: true,            // 디버그용
-    onEnter:     () => setBodyBG(colorClass),
-    onEnterBack: () => setBodyBG(colorClass),
-    onLeave:     () => document.body.classList.remove('white-bg','black-bg'),
-    onLeaveBack: () => document.body.classList.remove('white-bg','black-bg'),
-    invalidateOnRefresh: true,
-  });
-});
-
-// 레이아웃/이미지 로딩 후 재계산
-ScrollTrigger.refresh();
+$('[data-color]').each(function(){ 
+  color = ($(this).data('color') == '#fff') ? 'white' : 'black' // 만약 data-color = "#fff" 면 "white" 아니면 "black" -> 클래스명 변수
+  gsap.to('.background',{
+      scrollTrigger:{
+          trigger:"#contact",
+          start:"top 10%",
+          end:"bottom 10%",
+          scrub:1,
+          toggleClass: {targets: ".background", className: color},
+          markers:true
+      }
+  })
+})
 
 $(".sc-contact .sub-tit").mousemove(function(e){
   var x = ((-$(this).width() / 2) + e.offsetX) *0.3;      
