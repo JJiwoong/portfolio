8b7bd79e65605df22d388fc6fc0d54139e3cca0a
JJiwoong <jeongjw0520@naver.com>
2025-10-14 01:08:21 +0900

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -53,22 +53,33 @@ $('a').mouseleave(function(){
   gsap.to('.cursor2 span', { visibility: "hidden", opacity: 0,duration:0.1})
 })
 
+// 배경 클래스 교체를 항상 '배타적'으로
+function setBodyBG(cls) {
+  document.body.classList.remove('white-bg', 'black-bg');
+  document.body.classList.add(cls);
+}
+
 $('[data-color]').each(function (i, el) {
-  const colorValue = $(el).data('color'); // "#fff" or "#000"
+  const colorValue = $(el).data('color');          // "#fff" or "#000"
   const colorClass = (colorValue === '#fff') ? 'white-bg' : 'black-bg';
 
-  gsap.to('body', {
-    scrollTrigger: {
-      trigger: el,
-      start: "top 10%",
-      end: "bottom 10%",
-      scrub: 1,
-      toggleClass: { targets: "body", className: colorClass },
-            invalidateOnRefresh: true,
-            markers:true,
-    }
+  ScrollTrigger.create({
+    trigger: el,                 // ← 이 el은 data-color 가진 section (여기선 #contact)
+    start: "top 55%",            // 범위를 조금 안쪽으로 (너무 일찍 on 되는 것 방지)
+    end:   "bottom 45%",
+    scrub: false,                // 색 전환은 딱- 바뀌게, 원하면 1로
+    anticipatePin: 1,            // 상단 pin 섹션 영향 보정
+    // markers: true,            // 디버그용
+    onEnter:     () => setBodyBG(colorClass),
+    onEnterBack: () => setBodyBG(colorClass),
+    onLeave:     () => document.body.classList.remove('white-bg','black-bg'),
+    onLeaveBack: () => document.body.classList.remove('white-bg','black-bg'),
+    invalidateOnRefresh: true,
   });
 });
+
+// 레이아웃/이미지 로딩 후 재계산
+ScrollTrigger.refresh();
 
 $(".sc-contact .sub-tit").mousemove(function(e){
   var x = ((-$(this).width() / 2) + e.offsetX) *0.3;      
