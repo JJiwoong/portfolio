44413096f2b8615653b3589b8347bce59a3f2246
JJiwoong <jeongjw0520@naver.com>
2025-11-05 17:06:47 +0900

    header

--- js/main2.js
+++ js/main2.js
@@ -13,7 +13,7 @@ gsap.to("#page1", {
 });
 
 const button = document.querySelector(".button");
-const logo = document.querySelector(".logo");
+const logo = document.querySelector(".header__logo");
 
 // opacity 애니 끝난 후 z-index 낮추기
 [button, logo].forEach(el => {
